c3054f47b41bb14221674daa3bbbda8941504781
kashanismail93 <49834619+kashanismail93@users.noreply.github.com>
2019-04-22 00:31:54 +0500

    Add project to repository

---
+++ Test.java
@@ -0,0 +1,18 @@
+class Test{
+	void appointment(){
+		java.util.Scanner input = new java.util.Scanner(System.in);
+		java.util.Scanner inputS = new java.util.Scanner(System.in);
+		System.out.println("Dr Nouman Shaikh is avaiable at this time.");
+		System.out.println("For Appointment Press 1");
+		System.out.println("Not intreseted Press 2");
+		System.out.print("Make your choice:");
+		int choice=input.nextInt();
+		String doctorName = "Dr Nouman Shaikh";
+		String time = "11:00 AM";
+		String date = "1/1/19";
+		}
+
+
+}
+
+
